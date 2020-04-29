c1bbbb964ac0ba639f36248d1196cd8c015af1fd
mustaphakd <mustaphakd@yahoo.com>
2020-04-29 08:58:27 +0000

    rest support

---
+++ src/frontend-mobile/Makolo/Citizen/Rest/IClientApi.cs
@@ -0,0 +1,27 @@
+﻿using Backend.DataTransferObjects;
+using Refit;
+using Refit.Insane.PowerPack.Caching;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Citizen.Rest
+{
+    public interface IClientApi
+
+    {
+
+        [Post("/login")]
+        Task<AuthModel> Login(UserLogin details, CancellationToken cancellationToken = default(CancellationToken));
+
+        [Post("/security/register")]
+        Task register(UserRegistration registrationDetails, CancellationToken cancellationToken = default(CancellationToken));
+
+
+        // [RefitCache(1600)]
+
+
+    }
+}
